c5f1c556849b33a07656b6fe8548eff4d880fc13
CavemanKT <vkoubbeago@gmail.com>
2022-01-30 03:59:57 +0800

    deleted repeat define

--- src/components/auctions/Body.js
+++ src/components/auctions/Body.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext, useState} from 'react';
 import { AuthContext } from '../../context/AuthContext';
 import { useFirestore } from '../../hooks/useFirestore';
 import { AddAuction } from './AddAuction';
