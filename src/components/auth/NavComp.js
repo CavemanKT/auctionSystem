d26e5a0a960bb80cb391078a1a65920a80220e37
CavemanKT <vkoubbeago@gmail.com>
2022-01-30 14:24:43 +0800

    changed cookiePolicy

--- src/components/auth/NavComp.js
+++ src/components/auth/NavComp.js
@@ -69,7 +69,7 @@ export const NavComp = () => {
                 onSuccess={responseGoogle}
                 onFailure={responseGoogle}
                 isSignedIn={true}
-                cookiePolicy={'single_host_origin'}
+                cookiePolicy={process.env.REACT_APP_GOOGLE_AUTH_ORIGIN}
             />
             )
           }
